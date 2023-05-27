4a415d298b624b818c311936fc49f5e826b2087d
VPU <larind93@gmail.com>
2023-05-27 11:49:58 +0700

    Исправлены checkstyle ошибки

--- src/main/java/ru/yandex/practicum/filmorate/excption/ValidationException.java
+++ src/main/java/ru/yandex/practicum/filmorate/excption/ValidationException.java
@@ -1,6 +1,6 @@
 package ru.yandex.practicum.filmorate.excption;
 
-public class ValidationException extends Exception{
+public class ValidationException extends Exception {
     public ValidationException(String massage) {
         super(massage);
     }
